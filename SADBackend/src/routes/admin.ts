79e23178b472bc1f1aa95750b73b7f2f839e66bc
Karol Kierzkowski <karol.kierzkowski72@gmail.com>
2022-11-12 21:00:09 +0000

    admin 'post user' route done for admin panel

--- SADBackend/src/routes/admin.ts
+++ SADBackend/src/routes/admin.ts
@@ -1,21 +1,24 @@
 import { Router } from 'express';
 import AdminController from '../controllers/admin';
+import UserController from '../controllers/user';
 import { IRoute } from '../interfaces/routes';
 import AuthenticateAPIKeyRequest from '../middleware/admin';
 import ValidationMiddleware from '../middleware/validate';
 import { LoginRequest, RegisterRequest } from '../validation/auth';
+import { UserDecorated } from '../validation/user';
 
 export default class AdminRoute implements IRoute {
   public path = '/admin/';
   public router = Router();
   public adminController = new AdminController();
-
+  public userController = new UserController();
   constructor() {
     this.initializeRoutes();
   }
 
   private initializeRoutes() {
     this.router.post(`${this.path}createAdminUser`, AuthenticateAPIKeyRequest, ValidationMiddleware(RegisterRequest, 'body'), this.adminController.registerAdminUser);
+    this.router.post(`${this.path}users/`,  AuthenticateAPIKeyRequest, ValidationMiddleware(UserDecorated, 'body'), this.userController.PostUser);
     // this.router.post(`${this.path}`, ValidationMiddleware(LoginRequest, 'body'), this.authController.login);
   }
 }
