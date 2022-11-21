e601dde0e4c247ee1f6ff9bf7f41fdac16fe5484
DANNY OXBY <b9017792@hallam.shu.ac.uk>
2022-11-21 18:53:20 +0000

    couldn't get auth to work

--- SADBackend/src/test/test.user.ts
+++ SADBackend/src/test/test.user.ts
@@ -4,6 +4,7 @@ import chaiHttp from "chai-http"
 import e from 'express';
 import app from "../index"
 import { assert } from "console";
+import { stringify } from "querystring";
 
 chai.use(chaiHttp);
 chai.should();
@@ -16,80 +17,56 @@ chai.should();
   }
 }
 */
+var bearertoken: string;
 
-//https://dev.to/wakeupmh/debugging-mocha-tests-in-vscode-468a
-var bearertoken;
-
-before(function () {
-  describe('Get the auth token', () =>{
-    it('Find, admin bearer token', (done) =>{
-      chai.request(app)
-      .post('/api/auth/login')
-      .type('json')
-      .send({
-        "username" : "Danny",
-        "password" : "123"
-      })
-      .end((err, res) => { 
-        expect(res.body).to.be.a('object');
-        //try{
-        console.log(res + " --- " + res.type);
-        expect(res).to.have.status(200);
-        // expect(res).to.have.property("Response").with.property("token");
-        //   }
-        //   catch (expect){
-        //     console.log(err + " --- " + expect);
-        //   }
-        bearertoken = res.body.Response.token;
-       done(); //exit call
-      });
+before(function () { //don't put tests in here else the before will not be called first causing issues (same with describe and it)
+  chai.request(app)
+    .post('/api/auth/login')
+    .type('json')
+    .send({
+      "username": "Danny",
+      "password": "123"
+    })
+    .end((err, res) => {
+      bearertoken = res.body.Response.token; //save the value locally to use in later methods
+      console.log(bearertoken);
     });
-  });
 });
 
-// //the parent block
-// describe('Create A Tutor', () => {
-//     it('Should Create a Staff', (done) => {
-//         chai.request(app)
-//             .post('/api/users/resource')
-//             .type('json')
-//             .send({
-//               "username": "TestTutor",
-//                       "password": "123",
-//                       "roles" : ["Staff"],
-//                       "fullname": {
-//                         "firstname": "TestStaff",
-//                         "lastname": "Member"
-//                       },
-//                       "address": {
-//                         "addressLine1": "home",
-//                         "postcode": "12@bs",
-//                         "city": "sheffield",
-//                         "country": "UK"
-//                       }
-//             })
-//             .end((err, res) => {
-//                 //res.should.have.status(200); 
-//                 console.log("RES IS " + res);
-//                 console.log("Error IS " + err);
-//                 expect(res?.status).to.equal(200);
-//                 done();
-//             });
-//     });
+//these methods work under the assumption that the DB exists and that the roles Staff, Admin and Student exist
 
-// }); 
+//the parent block
+describe('Create Users', () => {
+  it('Should Create a Staff', (done) => {
+    chai.request(app)
+    .post('/api/users/resource')
+    .set("Authorization", `Bearer ${bearertoken}`) //error comes from auth.ts middleware line 28 - const payload - verify// 401
+    //.set({ "Authorization": `Bearer ${bearertoken}` })// 401
+    .type('json')
+    .send({
+      "username": "TestTutor",
+      "password": "123",
+      "roles": ["Staff"],
+      "fullname": {
+        "firstname": "TestStaff",
+        "lastname": "Member"
+      },
+        "address": {
+          "addressLine1": "home",
+          "postcode": "12@bs",
+          "city": "sheffield",
+          "country": "UK"
+        }
+      })
+      .end((err, res) => {
+        console.log("RES IS " + res);
+        console.log("Error IS " + err);
+        res.should.have.status(200);
+        done(); //end point
+      });
+  });
 
-
-// // import 'mocha'
-// // //const { expect } = require('chai');
-
-// import chai from "chai"
-// import chaiHttp from "chai-http"
-// import e from 'express';
-// import app from "../index"
-// let should = chai.should();
-
-// chai.use(chaiHttp);
+});
 
 // describe('Create A Tutor', () => {
 //   it("Should Create a Staff", (done) => {
