72876a227468a4c09a2347976122d582f9a07a52
DANNY OXBY <b9017792@hallam.shu.ac.uk>
2022-11-23 11:06:34 +0000

    fixed thundre delete and put user (resource lost)

--- SADBackend/src/test/test.user.ts
+++ SADBackend/src/test/test.user.ts
@@ -8,18 +8,16 @@ import { stringify } from "querystring";
 
 chai.use(chaiHttp);
 chai.should();
-/*
-{
-  "Success": true,
-  "Response": {
-    "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySUQiOiI2MzZjZjZkY2U3OWIxMjZkNmQyOTg5YmQiLCJpYXQiOjE2Njg3ODM4NjksImV4cCI6MTY2ODc5MTA2OX0.jLRN_7aXHicgYblQtDL620Tyct_j0bX6bEmWzLaFfM8",
-    "expiry": 1668791069
-  }
-}
-*/
 var bearertoken: string;
 
 before(function () { //don't put tests in here else the before will not be called first causing issues (same with describe and it)
+  /*{
+    "Success": true,
+    "Response": {
+      "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySUQiOiI2MzZjZjZkY2U3OWIxMjZkNmQyOTg5YmQiLCJpYXQiOjE2Njg3ODM4NjksImV4cCI6MTY2ODc5MTA2OX0.jLRN_7aXHicgYblQtDL620Tyct_j0bX6bEmWzLaFfM8",
+      "expiry": 1668791069
+    }
+  }*/
   chai.request(app)
     .post('/api/auth/login')
     .type('json')
@@ -36,21 +34,25 @@ before(function () { //don't put tests in here else the before will not be calle
 //these methods work under the assumption that the DB exists and that the roles Staff, Admin and Student exist
 
 //the parent block
-describe('Create Users', () => {
-  it('Should Create a Staff', (done) => {
+describe('Test Users', () => {
+  /*{
+    "Success": true,
+    "Response": "637dfab875f042c13c3aa209"
+  }*/
+  var tutorid: string;
+  it('Create a Staff', (done) => {
     chai.request(app)
-    .post('/api/users/resource')
-    .set("Authorization", `Bearer ${bearertoken}`) //error comes from auth.ts middleware line 28 - const payload - verify// 401
-    //.set({ "Authorization": `Bearer ${bearertoken}` })// 401
-    .type('json')
-    .send({
-      "username": "TestTutor",
-      "password": "123",
-      "roles": ["Staff"],
-      "fullname": {
-        "firstname": "TestStaff",
-        "lastname": "Member"
-      },
+      .post('/api/users/resource') //rout path
+      .set({ "Authorization": `Bearer ${bearertoken}` }) // pass the login token from before method
+      .type('json')
+      .send({
+        "username": "TestTutor",
+        "password": "123",
+        "roles": ["Staff"],
+        "fullname": {
+          "firstname": "TestStaff",
+          "lastname": "Member"
+        },
         "address": {
           "addressLine1": "home",
           "postcode": "12@bs",
@@ -59,41 +61,22 @@ describe('Create Users', () => {
         }
       })
       .end((err, res) => {
-        console.log("RES IS " + res);
-        console.log("Error IS " + err);
         res.should.have.status(200);
+        res.body.should.have.property("Success").equal(true);
+        res.body.should.have.property("Response");
+        tutorid = res.body.Response;
         done(); //end point
       });
   });
 
+  it('Delete a Staff', (done) => {
+    chai.request(app)
+      .post(`/users/resource/${tutorid}`) //rout path
+      .set({ "Authorization": `Bearer ${bearertoken}` }) // pass the login token from before method
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.have.property("Success").equal("true");
+        done(); //end point
+      });
+  });
 });
-
-// describe('Create A Tutor', () => {
-//   it("Should Create a Staff", (done) => {
-//     chai.request(app)
-//     .post("users/resource/") //post holds the parameter information
-//     .send({ //send holds hte body infomration
-//       "username": "TestTutor",
-//         "password": "123",
-//         "roles" : ["Staff"],
-//         "fullname": {
-//           "firstname": "TestStaff",
-//           "lastname": "Member"
-//         },
-//         "address": {
-//           "addressLine1": "home",
-//           "postcode": "12@bs",
-//           "city": "sheffield",
-//           "country": "UK"
-//         }
-//     })
-//     .end((err, res) => { //check the retrund value
-//       res.should.have.status(200);
-//       res.should.have.status(200); //successful creation
-//       res.should.have.a("object"); //should return a user object
-//       res.should.have.property("Success").equal("true");
-//       res.should.have.property("Response");
-//       done(); //end point
-//     });
-//   });
-// });
