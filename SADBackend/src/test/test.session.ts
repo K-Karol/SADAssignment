3c9c897a1693eb36902dfb8fe5ad825cee8d5f2c
DANNY OXBY <b9017792@hallam.shu.ac.uk>
2022-11-24 16:59:39 +0000

    all tests now create a valid admin beforehand

--- SADBackend/src/test/test.session.ts
+++ SADBackend/src/test/test.session.ts
@@ -12,6 +12,7 @@ var bearertoken: string; //auth token
 var tutorid: string;
 var modleadid: string;
 var moduleid: string;
+var admin: string;
 var studentids: string[] = []; //create users
 
 //before << do something at the start of the file
@@ -20,50 +21,49 @@ var studentids: string[] = []; //create users
 //afterEach << do something after each function
 
 before(function (done) { //do first
-  chai.request(app) //login as a admin
-    .post('/api/auth/login')
-    .type('json')
+  chai.request(app)
+    .post("/api/admin/createAdminUser")
+    .set('X-API-Key', `${process.env.ADMIN_APIKEY}`)
+    .type("json")
     .send({
-      "username": "Danny",
-      "password": "123"
+      "username": "Testersession",
+      "password": "123",
+      "fullname": {
+        "firstname": "Firstname",
+        "lastname": "Lastname"
+      },
+      "address": {
+        "addressLine1": "test address",
+        "postcode": "abcde",
+        "city": "sheffield",
+        "country": "UK"
+      }
     })
-    .then(res => {
-      bearertoken = res.body.Response.token; //save the value locally to use in later methods
+    .end((err, res) => {
+      res.should.have.status(200);
+      admin = res.body.Response;
 
-      chai.request(app) //create tutor 
-        .post('/api/users/resource')
-        .set({ "Authorization": `Bearer ${bearertoken}` }) // pass the login token from before method
+      chai.request(app) //login as a admin
+        .post('/api/auth/login')
         .type('json')
         .send({
-          "username": "TestTutorsessiontest",
-          "password": "123",
-          "roles": ["Staff"],
-          "fullname": {
-            "firstname": "TestStaff",
-            "lastname": "Member"
-          },
-          "address": {
-            "addressLine1": "home",
-            "postcode": "12@bs",
-            "city": "sheffield",
-            "country": "UK"
-          }
+          "username": "Testersession",
+          "password": "123"
         })
-        .end((err, res) => {
-          tutorid = res.body.Response;
-          //console.log(tutorid);
+        .then(res => {
+          bearertoken = res.body.Response.token; //save the value locally to use in later methods
 
-          chai.request(app) //create module leader staff
-            .post('/api/users/resource') //route path
+          chai.request(app) //create tutor 
+            .post('/api/users/resource')
             .set({ "Authorization": `Bearer ${bearertoken}` }) // pass the login token from before method
             .type('json')
             .send({
-              "username": "TestModLeadersessiontest",
-              "password": "123456",
+              "username": "TestTutorsessiontest",
+              "password": "123",
               "roles": ["Staff"],
               "fullname": {
-                "firstname": "Mod",
-                "lastname": "Lead"
+                "firstname": "TestStaff",
+                "lastname": "Member"
               },
               "address": {
                 "addressLine1": "home",
@@ -73,20 +73,20 @@ before(function (done) { //do first
               }
             })
             .end((err, res) => {
-              modleadid = res.body.Response;
-              //console.log(modleadid);
+              tutorid = res.body.Response;
+              //console.log(tutorid);
 
-              chai.request(app) //create student 1
+              chai.request(app) //create module leader staff
                 .post('/api/users/resource') //route path
                 .set({ "Authorization": `Bearer ${bearertoken}` }) // pass the login token from before method
                 .type('json')
                 .send({
-                  "username": `Student1sessiontest`,
+                  "username": "TestModLeadersessiontest",
                   "password": "123456",
-                  "roles": [],
+                  "roles": ["Staff"],
                   "fullname": {
-                    "firstname": "Stu",
-                    "lastname": `1`
+                    "firstname": "Mod",
+                    "lastname": "Lead"
                   },
                   "address": {
                     "addressLine1": "home",
@@ -96,19 +96,20 @@ before(function (done) { //do first
                   }
                 })
                 .end((err, res) => {
-                  studentids.push(res.body.Response);
+                  modleadid = res.body.Response;
+                  //console.log(modleadid);
 
-                  chai.request(app) //create student 2
-                    .post('/api/users/resource')
+                  chai.request(app) //create student 1
+                    .post('/api/users/resource') //route path
                     .set({ "Authorization": `Bearer ${bearertoken}` }) // pass the login token from before method
                     .type('json')
                     .send({
-                      "username": `Student2sessiontest`,
+                      "username": `Student1sessiontest`,
                       "password": "123456",
                       "roles": [],
                       "fullname": {
                         "firstname": "Stu",
-                        "lastname": `2`
+                        "lastname": `1`
                       },
                       "address": {
                         "addressLine1": "home",
@@ -120,17 +121,17 @@ before(function (done) { //do first
                     .end((err, res) => {
                       studentids.push(res.body.Response);
 
-                      chai.request(app) //create student 3
-                        .post('/api/users/resource') //route path
+                      chai.request(app) //create student 2
+                        .post('/api/users/resource')
                         .set({ "Authorization": `Bearer ${bearertoken}` }) // pass the login token from before method
                         .type('json')
                         .send({
-                          "username": `Student3sessiontest`,
+                          "username": `Student2sessiontest`,
                           "password": "123456",
                           "roles": [],
                           "fullname": {
                             "firstname": "Stu",
-                            "lastname": `3`
+                            "lastname": `2`
                           },
                           "address": {
                             "addressLine1": "home",
@@ -142,27 +143,50 @@ before(function (done) { //do first
                         .end((err, res) => {
                           studentids.push(res.body.Response);
 
-                          chai.request(app) //create session
-                            .post(`/api/modules/resource`)
+                          chai.request(app) //create student 3
+                            .post('/api/users/resource') //route path
                             .set({ "Authorization": `Bearer ${bearertoken}` }) // pass the login token from before method
                             .type('json')
                             .send({
-                              "name": "WTFYMNMT",
-                              "year": "2022",
-                              "semester": "SEM1",
-                              "students": [`${studentids[0]}`, `${studentids[1]}`, `${studentids[2]}`],
-                              "cohorts": [
-                                {
-                                  "identifier": "group 1",
-                                  "students": [`${studentids[0]}`, `${studentids[1]}`,]
-                                },
-                              ],
-                              "moduleLeader": `${modleadid}`,
-                              "instructors": [`${modleadid}`, `${tutorid}`]
+                              "username": `Student3sessiontest`,
+                              "password": "123456",
+                              "roles": [],
+                              "fullname": {
+                                "firstname": "Stu",
+                                "lastname": `3`
+                              },
+                              "address": {
+                                "addressLine1": "home",
+                                "postcode": "12@bs",
+                                "city": "sheffield",
+                                "country": "UK"
+                              }
                             })
                             .end((err, res) => {
-                              moduleid = res.body.Response;
-                              done();
+                              studentids.push(res.body.Response);
+
+                              chai.request(app) //create session
+                                .post(`/api/modules/resource`)
+                                .set({ "Authorization": `Bearer ${bearertoken}` }) // pass the login token from before method
+                                .type('json')
+                                .send({
+                                  "name": "WTFYMNMT",
+                                  "year": "2022",
+                                  "semester": "SEM1",
+                                  "students": [`${studentids[0]}`, `${studentids[1]}`, `${studentids[2]}`],
+                                  "cohorts": [
+                                    {
+                                      "identifier": "group 1",
+                                      "students": [`${studentids[0]}`, `${studentids[1]}`,]
+                                    },
+                                  ],
+                                  "moduleLeader": `${modleadid}`,
+                                  "instructors": [`${modleadid}`, `${tutorid}`]
+                                })
+                                .end((err, res) => {
+                                  moduleid = res.body.Response;
+                                  done();
+                                });
                             });
                         });
                     });
@@ -230,22 +254,22 @@ describe("Attendence", () => {
       .set({ "Authorization": `Bearer ${bearertoken}` }) // pass the login token from before method
       .type('json')
       .send({
-          "attendance" : "late"
+        "attendance": "late"
       })
       .end((err, res) => {
         res.should.have.status(200);
 
         //chack that change has shown
         chai.request(app) //check the users current attendence
-        .get(`/api/sessions/GetUserAttendence/${sessionid}/${studentids[0]}`)
-        .set({ "Authorization": `Bearer ${bearertoken}` }) // pass the login token from before method
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.have.property("Success").equal(true);
-          res.body.should.have.property("Response").property("username").eql("Student1sessiontest");
-          res.body.should.have.property("Response").property("attendance").eql("late");
-          done();
-        });
+          .get(`/api/sessions/GetUserAttendence/${sessionid}/${studentids[0]}`)
+          .set({ "Authorization": `Bearer ${bearertoken}` }) // pass the login token from before method
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.have.property("Success").equal(true);
+            res.body.should.have.property("Response").property("username").eql("Student1sessiontest");
+            res.body.should.have.property("Response").property("attendance").eql("late");
+            done();
+          });
       });
   });
 
@@ -263,7 +287,7 @@ describe("Attendence", () => {
   });
 });
 
-after(async function () { //delete the create object in before method
+after(function (done) { //delete the create object in before method
   chai.request(app)
     .delete(`/api/users/resource/${tutorid}`) //remove tutor
     .set({ "Authorization": `Bearer ${bearertoken}` }) // pass the login token from before method
@@ -289,4 +313,12 @@ after(async function () { //delete the create object in before method
     .set({ "Authorization": `Bearer ${bearertoken}` }) // pass the login token from before method
     .end((err, res) => {
     });
+
+  chai.request(app)
+    .delete(`/api/users/resource/${admin}`) //route path
+    .set({ "Authorization": `Bearer ${bearertoken}` }) // pass the login token from before method
+    .end((err, res) => {
+      done();
+    });
+
 });
