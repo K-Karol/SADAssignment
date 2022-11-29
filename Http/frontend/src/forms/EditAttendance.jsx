d0c137ca2181401b6c971d38dc380c7eddd3987f
REECE WAREHAM <c0024660@hallam.shu.ac.uk>
2022-11-29 22:13:46 +0000

    Edit attendance progress

--- Http/frontend/src/forms/EditAttendance.jsx
+++ Http/frontend/src/forms/EditAttendance.jsx
@@ -1,15 +1,20 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { fetchToken } from "../store";
-import {Button} from "@mui/material";
+import {Button, Stack} from "@mui/material";
+import {Form, Field, Formik} from "formik";
 
 export default function EditAttendance() {
 
     const [sessionID, setSessionID] = useState();
     const [studentID, setStudentID] = useState();
     const [attendance, setAttendance] = useState();
+    const [sessionList, setSessionList] = useState([]);
+    const [studentsList, setStudentsList] = useState([]);
+    const [session, setSession] = useState("");
+    const [student, setStudent] = useState("");
+
 
     const editAttendance = async () => {
-        console.log(attendance)
         var newAttendanceRequest = await fetch (`${window.location.origin}/api/sessions/PatchUserAttendence/${sessionID}/${studentID}`, {
             method: "PATCH",
             headers: {
@@ -17,46 +22,75 @@ export default function EditAttendance() {
                 Authorization: `Bearer ${fetchToken().token}`,
             },
             body: JSON.stringify({
-                attendance: attendance,
+                attendance: attendance
             })
         })
         .then(response => {console.log(response.status)
             return response.json();})
         .then(data=> console.log(data));
-        var newAttendance = await newAttendanceRequest.json();
     };
 
-    const handleSubmit = async e => {
-        var c = await editAttendance();
-    }
+    console.log(`Bearer ${fetchToken().token}`);
+  
+  useEffect(() => {
+    fetch(`${window.location.origin}/api/sessions/resource`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${fetchToken().token}`
+      },
+    }).then((res) => res.json())
+    .then((sessions) => {
+      if (sessions.Success) {
+        setSessionList(sessions.Response.sessions);
+      }
+    });
+  }, []);
 
     return (
-        <div className="EditAttendance">
-            <h1> Manually edit attendance, form goes here</h1>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    <p>Session ID</p>
-                    <input type="text" onChange={e => setSessionID(e.target.value)}/>
-                </label>
-                <label>
-                    <p>Student ID</p>
-                    <input type="text" onChange={e => setStudentID(e.target.value)}/>
-                </label>
-                <label>
-                    <p>Attendance</p>
-                    <select name="Attendance" onChange={e => setAttendance(e.target.value)}>Attendance
+
+    <div className="EditAttendance">
+        <Formik
+            initialValues={{
+                sessionField: '',
+                studentField: '',
+                attendanceField: ''
+            }}
+            onSubmit={async (values) => {
+                console.log("test");
+                await new Promise((r) => setTimeout(r, 500));
+                sessionID = values.sessionField;
+                studentID = values.studentField;
+                attendance = values.attendanceField;
+                var c = await editAttendance();
+            }}
+        >
+
+            <Form>
+                <Stack spacing={1}>
+                    <label htmlFor="sessionField">Session</label>
+                    <Field as="select" name="sessionField">
+                        <option value=""></option>
+                        {sessionList.map((session) => <option value={session._id}>{session._id}</option>)}
+                    </Field>
+                    <label htmlFor="studentField">Student</label>
+                    <Field as="select" name="studentField">
+                        <option value=""></option>
+                        {studentsList.map((student) => <option value={student._id}>{student._id}</option>)}
+                    </Field>
+                    <label htmlFor="attendanceField">Attendance</label>
+                    <Field as="select" name="attendanceField">
                         <option value="late">Late</option>
                         <option value="not">Not</option>
                         <option value="full">Full</option>
-                    </select>
-                </label>
-                <div>
-                    <Button variant="contained" type="submit">Submit</Button>
-                </div>
-            </form>
-        </div>
-
-    )
+                    </Field>
+                    <button type="submit"> Edit Attendance </button>
+                </Stack>
+            </Form>
+        </Formik>
+    </div>
+    );
 }
 // do the extend period thing here
 // extenuating circumstances etc - easy to do with API call
