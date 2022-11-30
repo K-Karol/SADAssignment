8a4e2c3922e2f5acd38c2ab3d9db5bfd837e671b
Casey Henderson <caseyhenderson13@gmail.com>
2022-11-30 14:59:50 +0000

    SAD-014: Logout and RBAC implemented, added Unauthorised page. Generate Code page protected. Updated manifest.json.

--- Http/frontend/src/pages/Logout.jsx
+++ Http/frontend/src/pages/Logout.jsx
@@ -1,15 +1,17 @@
 import { Button } from '@mui/material'
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux'
 
 // Not fully functional just yet - to be fixed in next commit
 // this just shows you how it would be done.
 // token removal and logout works fine
 // logging back in is the problem - fix should be relatively simple. 
 export default function Logout() {
+    const dispatch = useDispatch();
     const handleClick = event => {
         // 👇️ toggle shown state
-          localStorage.removeItem('token')
-          ;
+          localStorage.removeItem('token');
+          dispatch({type: "logout", payload: {} });
     };
     return(
         <div>
